ff41fd09e601e2b8464f109e316fc73456bf3756
Andrew Mina <andrewmina13@gmail.com>
2022-09-10 15:58:06 -0500

    a

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def print_hi(name):
+def convolution(im, kernal):
     # Use a breakpoint in the code line below to debug your script.
 
 
@@ -10,6 +10,8 @@ def print_hi(name):
 kernel = np.array([[1,1,1],
                    [1,1,1],
                    [1,1,1]])/9
+
+
 
 im = cv2.imread("lena.png")
 
